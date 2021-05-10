9540ebb8f66297944d179ef24ed3e8d0831943c1
Ricardo Gomes <sleiph@gmail.com>
2021-05-10 20:46:50 -0300

    o inicio de um sonho

---
+++ src/main/java/com/dio/heroisdio/HeroisdioApplication.java
@@ -0,0 +1,13 @@
+package com.dio.heroisdio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroisdioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroisdioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio/heroisdio/HeroisdioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.heroisdio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroisdioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
