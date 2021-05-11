64329b5f7aaf2dc07c51894d8ed2d16a04f96321
Ricardo Gomes <sleiph@gmail.com>
2021-05-10 23:04:39 -0300

    fim da aula 8

--- src/main/java/com/dio/heroisdio/HeroisdioApplication.java
+++ src/main/java/com/dio/heroisdio/HeroisdioApplication.java
@@ -2,12 +2,16 @@ package com.dio.heroisdio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 
 @SpringBootApplication
+@EnableDynamoDBRepositories
 public class HeroisdioApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(HeroisdioApplication.class, args);
+		System.out.println("Eu gosto mais de webflux q de heróis");
 	}
 
 }
